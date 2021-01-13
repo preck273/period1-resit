6ad94d655ea1adfcdb614d032f0b8589a12d4ace
samysosa1 <73364324+samysosa1@users.noreply.github.com>
2021-01-13 23:47:00 +0100

    Add files via upload
    homepage - still need some changes

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>homepage</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div>
+			<header class="theheader">
+			<img src="img/logo3.jpg" alt="logo picture" class="logo">
+				<nav class="thenav">
+					<a href="#">Home</a>
+					<div class="tracks">
+						<button class="trackbtn">Tracks</button>
+						<div class="track-menu">
+							<a href="#">Italian</a>
+							<a href="#">Asian</a>
+							<a href="#">East-European</a>
+						</div>
+					</div>
+					<a href="#">Sign-up</a>
+					<a href="#">Login</a>
+				</nav>
+				<h1 class="title">Culinary School</h1>
+				<span class="slogan">
+					<p><i>A trusted place where your turn your cooking passion into a culinary career with in-demand skills </i></p>
+				</span>
+			</header>
+			<main>
+				<section class="homesection1">
+					<h2 class="headstyle">Become a chef today</h2>
+					<img class="imagestyle1" src="img/header-img.jpg" alt="">
+					<p class="infostyle">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+					<img class="imagestyle1" src="img/cooking1.jpg" alt="">
+					<p class="infostyle">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>					
+				</section>
+				<section class="homesection2">
+					<h2 class="headstyle">Sign up today and enroll in one of our amazing culinary tracks!!!</h2>
+					<div class="track1">
+						<div class="track-image1">
+							<img src="img/italian-track.jpeg" alt="" class="image">
+							<div class="text-position">
+								<div class="text">
+									ITALIAN
+								</div>
+							</div>
+						</div>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+					</div>
+					<div class="track1">
+						<div class="track-image1">
+							<img src="img/asian-track.jpeg" alt="" class="image">
+							<div class="text-position">
+								<div class="text">
+									ASIAN
+								</div>
+							</div>
+						</div>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+					</div>
+					<div class="track1">
+						<div class="track-image1">
+							<img src="img/eastern-track.jpeg" alt="" class="image">
+							<div class="text-position">
+								<div class="text">
+									EASTERN EUROPEAN
+								</div>
+							</div>
+						</div>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+						<p class="infostyle2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
+					</div>
+					<img src="" alt="" class="">
+					<img src="" alt="" class="">
+				</section>
+			</main>
+			<footer class="mainfooter">
+				<div class="footerinfo1">&copy;2021</div>
+				<span class="footerinfo">
+					<p>Contact Us</p>
+					<p>Culinaryschool@gmail.com</p>
+					<p>1234567890</p>
+					<img src="img/icons/instagram.png" alt="" class="icon">
+					<img src="img/icons/facebook.png" alt="" class="icon">
+					<img src="img/icons/twitter.png" alt="" class="icon">
+				</span>
+				<span class="footerinfo2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</span>				
+			</footer>
+		</div>
+	</body>
+</html>
