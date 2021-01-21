e1eb7790c1921b42ee05791d1722a572a7e43639
samysosa1 <73364324+samysosa1@users.noreply.github.com>
2021-01-21 11:38:12 +0100

    updated the track pages

---
+++ includes/trackdata.php
@@ -0,0 +1,165 @@
+<?php
+	// Multi-dimentional array to store info on all three tracks
+		$tracksinfo = [];
+		$tracksinfo['italian'] = [
+			"pageTitle" => "Italian Track",
+			"pageHeading" =>"The Italian Track",
+			"track-heading" => "Welcome to the Italian track",
+			"image-position1" => "img/italia1.jpg",
+			"image-position2" => "img/italia2.jpg",
+			"subheading1" => "Vivamus a tellus",
+			"subheading2" => "Voluptate velit",
+			"subheading3" => "Ut enim ad minim veniam",
+			"subheading4" => "minim veniam",
+			"subheading5" => "semper risus",
+			"para1"       => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna",
+					
+			"para2"		  => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+						
+			"para3"       => "Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+					Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. 
+					Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. 
+					Ut nonummy.",
+			"para4"       => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+						
+			"course-para1" => "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+					
+			"course-para2" => "Tincidunt arcu non sodales neque sodales ut etiam. In nisl nisi scelerisque eu ultrices vitae auctor. 
+				Velit egestas dui id ornare. Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Eget felis eget nunc lobortis mattis. 
+				Pretium viverra suspendisse potenti nullam ac tortor vitae. Ac auctor augue mauris augue neque gravida in fermentum. 
+				Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Varius quam quisque id diam vel. Est lorem ipsum dolor sit amet.
+				Eu feugiat pretium nibh ipsum. In iaculis nunc sed augue lacus viverra. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.",
+				
+			"course-para3" => "Orci eu lobortis elementum nibh tellus molestie. Cursus in hac habitasse platea dictumst quisque sagittis purus. Tellus in metus vulputate eu. 
+				Rhoncus urna neque viverra justo nec ultrices dui sapien. Luctus accumsan tortor posuere ac ut consequat. Quam adipiscing vitae proin sagittis. 
+				Pellentesque habitant morbi tristique senectus. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Cursus metus aliquam eleifend mi in nulla. 
+				Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Et malesuada fames ac turpis egestas sed tempus urna et.",
+				
+			"course-para4" => "Pretium viverra suspendisse potenti nullam. Id semper risus in hendrerit gravida rutrum quisque non. Accumsan lacus vel facilisis volutpat est velit. 
+				Aliquet eget sit amet tellus cras adipiscing enim. Sollicitudin aliquam ultrices sagittis orci a. Tortor dignissim convallis aenean et tortor at. 
+				Neque gravida in fermentum et sollicitudin ac orci. Etiam non quam lacus suspendisse faucibus. Egestas erat imperdiet sed euismod nisi porta lorem.",
+			
+			"course-para5" => "Mattis molestie a iaculis at. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Est ullamcorper eget nulla facilisi etiam dignissim. 
+				Morbi tincidunt augue interdum velit euismod. Elit sed vulputate mi sit amet mauris commodo. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. 
+				Praesent tristique magna sit amet purus gravida. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Condimentum vitae sapien pellentesque habitant morbi."
+			];
+			
+		$tracksinfo['asian'] = [
+			"pageTitle" => "Asian Track",
+			"pageHeading" =>"The Asian Track",
+			"track-heading" => "Welcome to the Asian track",
+			"background =>" => "asianbackground1",
+			"image-position1" => "img/asia1.jpg",
+			"image-position2" => "img/asia2.jpg",
+			"subheading1" => "sodales ut etiam",
+			"subheading2" => "habitasse platea dictumst",
+			"subheading3" => "Mattis molestie",
+			"subheading4" => "Ac auctor augue mauris",
+			"subheading5" => "consectetur adipiscing",
+			"background1" => "asianbackground2",
+			"arrow"       => "asianarrow",
+			"para1"       => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna",
+					
+			"para2"		  => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+						
+			"para3"       => "Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+					Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. 
+					Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. 
+					Ut nonummy.",
+			"para4"       => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+						
+			"course-para1" => "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+					
+			"course-para2" => "Tincidunt arcu non sodales neque sodales ut etiam. In nisl nisi scelerisque eu ultrices vitae auctor. 
+				Velit egestas dui id ornare. Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Eget felis eget nunc lobortis mattis. 
+				Pretium viverra suspendisse potenti nullam ac tortor vitae. Ac auctor augue mauris augue neque gravida in fermentum. 
+				Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Varius quam quisque id diam vel. Est lorem ipsum dolor sit amet.
+				Eu feugiat pretium nibh ipsum. In iaculis nunc sed augue lacus viverra. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.",
+				
+			"course-para3" => "Orci eu lobortis elementum nibh tellus molestie. Cursus in hac habitasse platea dictumst quisque sagittis purus. Tellus in metus vulputate eu. 
+				Rhoncus urna neque viverra justo nec ultrices dui sapien. Luctus accumsan tortor posuere ac ut consequat. Quam adipiscing vitae proin sagittis. 
+				Pellentesque habitant morbi tristique senectus. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Cursus metus aliquam eleifend mi in nulla. 
+				Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Et malesuada fames ac turpis egestas sed tempus urna et.",
+				
+			"course-para4" => "Pretium viverra suspendisse potenti nullam. Id semper risus in hendrerit gravida rutrum quisque non. Accumsan lacus vel facilisis volutpat est velit. 
+				Aliquet eget sit amet tellus cras adipiscing enim. Sollicitudin aliquam ultrices sagittis orci a. Tortor dignissim convallis aenean et tortor at. 
+				Neque gravida in fermentum et sollicitudin ac orci. Etiam non quam lacus suspendisse faucibus. Egestas erat imperdiet sed euismod nisi porta lorem.",
+			
+			"course-para5" => "Mattis molestie a iaculis at. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Est ullamcorper eget nulla facilisi etiam dignissim. 
+				Morbi tincidunt augue interdum velit euismod. Elit sed vulputate mi sit amet mauris commodo. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. 
+				Praesent tristique magna sit amet purus gravida. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Condimentum vitae sapien pellentesque habitant morbi."
+			];
+		
+		$tracksinfo['eastern'] = [
+			"pageTitle" => "Eastern European Track",
+			"pageHeading" =>"The Eastern European Track",
+			"track-heading" => "Welcome to the Eastern European track",
+			"background =>" => "easternbackground1",
+			"image-position1" => "img/eastern1.jpg",
+			"image-position2" => "img/eastern2.jpg",
+			"subheading1" => "mauris commodo",
+			"subheading2" => "Tortor dignissim convallis",
+			"subheading3" => "ultrices sagittis orci",
+			"subheading4" => "voluptate velit esse",
+			"subheading5" => "commodo nulla facilisi",
+			"background1" => "easternbackground2",
+			"arrow"       => "easternarrow",
+			"para1"       => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
+					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna",
+					
+			"para2"		  => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+						
+			"para3"       => "Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+					Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. 
+					Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. 
+					Ut nonummy.",
+			"para4"       => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+						
+			"course-para1" => "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+					
+			"course-para2" => "Tincidunt arcu non sodales neque sodales ut etiam. In nisl nisi scelerisque eu ultrices vitae auctor. 
+				Velit egestas dui id ornare. Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Eget felis eget nunc lobortis mattis. 
+				Pretium viverra suspendisse potenti nullam ac tortor vitae. Ac auctor augue mauris augue neque gravida in fermentum. 
+				Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Varius quam quisque id diam vel. Est lorem ipsum dolor sit amet.
+				Eu feugiat pretium nibh ipsum. In iaculis nunc sed augue lacus viverra. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.",
+				
+			"course-para3" => "Orci eu lobortis elementum nibh tellus molestie. Cursus in hac habitasse platea dictumst quisque sagittis purus. Tellus in metus vulputate eu. 
+				Rhoncus urna neque viverra justo nec ultrices dui sapien. Luctus accumsan tortor posuere ac ut consequat. Quam adipiscing vitae proin sagittis. 
+				Pellentesque habitant morbi tristique senectus. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Cursus metus aliquam eleifend mi in nulla. 
+				Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Et malesuada fames ac turpis egestas sed tempus urna et.",
+				
+			"course-para4" => "Pretium viverra suspendisse potenti nullam. Id semper risus in hendrerit gravida rutrum quisque non. Accumsan lacus vel facilisis volutpat est velit. 
+				Aliquet eget sit amet tellus cras adipiscing enim. Sollicitudin aliquam ultrices sagittis orci a. Tortor dignissim convallis aenean et tortor at. 
+				Neque gravida in fermentum et sollicitudin ac orci. Etiam non quam lacus suspendisse faucibus. Egestas erat imperdiet sed euismod nisi porta lorem.",
+			
+			"course-para5" => "Mattis molestie a iaculis at. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Est ullamcorper eget nulla facilisi etiam dignissim. 
+				Morbi tincidunt augue interdum velit euismod. Elit sed vulputate mi sit amet mauris commodo. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. 
+				Praesent tristique magna sit amet purus gravida. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Condimentum vitae sapien pellentesque habitant morbi."
+			];
+?>
--- trackcontent.php
+++ trackcontent.php
@@ -1,49 +1,5 @@
 	<?php 
-		// Multi-dimentional array to store info on all three tracks
-		$tracksinfo = [];
-		$tracksinfo['italian'] = [
-			"pageTitle" => "Italian Track",
-			"pageHeading" =>"The Italian Track",
-			"track-heading" => "Welcome to the Italian track",
-			"image-position1" => "img/italia1.jpg",
-			"image-position2" => "img/italia2.jpg",
-			"subheading1" => "Vivamus a tellus",
-			"subheading2" => "Voluptate velit",
-			"subheading3" => "Ut enim ad minim veniam",
-			"subheading4" => "minim veniam",
-			"subheading5" => "semper risus"];
-			
-		$tracksinfo['asian'] = [
-			"pageTitle" => "Asian Track",
-			"pageHeading" =>"The Asian Track",
-			"track-heading" => "Welcome to the Asian track",
-			"background =>" => "asianbackground1",
-			"image-position1" => "img/asia1.jpg",
-			"image-position2" => "img/asia2.jpg",
-			"subheading1" => "sodales ut etiam",
-			"subheading2" => "habitasse platea dictumst",
-			"subheading3" => "Mattis molestie",
-			"subheading4" => "Ac auctor augue mauris",
-			"subheading5" => "consectetur adipiscing",
-			"background1" => "asianbackground2",
-			"arrow"       => "asianarrow"];
-		
-		$tracksinfo['eastern'] = [
-			"pageTitle" => "Eastern European Track",
-			"pageHeading" =>"The Eastern European Track",
-			"track-heading" => "Welcome to the Eastern European track",
-			"background =>" => "easternbackground1",
-			"image-position1" => "img/eastern1.jpg",
-			"image-position2" => "img/eastern2.jpg",
-			"subheading1" => "mauris commodo",
-			"subheading2" => "Tortor dignissim convallis",
-			"subheading3" => "ultrices sagittis orci",
-			"subheading4" => "voluptate velit esse",
-			"subheading5" => "commodo nulla facilisi",
-			"background1" => "easternbackground2",
-			"arrow"       => "easternarrow"];
-			
-			
+		include("includes/trackdata.php"); 
 		$track = "italian";			
 		// using the $_GET to call the $page variable when clicked on a track then use statements to sort the information.
 		if($_GET["page"] == "italian"){
@@ -60,7 +16,8 @@
 		$pageHeading = $tracksinfo[$track]["pageHeading"];	
 		$background = false;
 		
-		include("includes/header.php"); ?>
+		include("includes/header.php"); 
+	?>
 		
 		<main>
 			<h2 class="track-heading"><?php echo $tracksinfo[$track]["track-heading"]; ?></h2>
@@ -68,24 +25,14 @@
 				<div class="info-blocka">
 					<img src="<?php echo $tracksinfo[$track]["image-position1"]; ?>" alt="food" class="image-position">
 					<div  class="info-position">
-						<p class="para-style">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere,
-					magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna</p>
-						<p class="para-style">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<p class="para-style"><?php echo $tracksinfo[$track]["para1"]; ?></p>
+						<p class="para-style"><?php echo $tracksinfo[$track]["para2"]; ?></p>
 					</div>
 				</div>
 				<div class="info-blockb">
 					<div class="info-position">
-						<p class="para-style">Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-					Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. 
-					Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. 
-					Ut nonummy.</p>
-						<p class="para-style">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-						Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<p class="para-style"><?php echo $tracksinfo[$track]["para3"]; ?></p>
+						<p class="para-style"><?php echo $tracksinfo[$track]["para4"]; ?></p>
 					</div>
 					<img src="<?php echo $tracksinfo[$track]["image-position2"]; ?>" alt="food" class="image-position">
 				</div>				
@@ -93,32 +40,19 @@
 			<section>
 				<h2 class="track-heading">List of courses you can expect from this track</h2>
 				<h3 class="subheading"><?php echo $tracksinfo[$track]["subheading1"]; ?></h3>
-				<p class="para-style">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-					nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+				<p class="para-style"><?php echo $tracksinfo[$track]["course-para1"]; ?></p>
 					
 				<h3 class="subheading"><?php echo $tracksinfo[$track]["subheading2"]; ?></h3>
-				<p class="para-style">Tincidunt arcu non sodales neque sodales ut etiam. In nisl nisi scelerisque eu ultrices vitae auctor. 
-				Velit egestas dui id ornare. Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Eget felis eget nunc lobortis mattis. 
-				Pretium viverra suspendisse potenti nullam ac tortor vitae. Ac auctor augue mauris augue neque gravida in fermentum. 
-				Quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Varius quam quisque id diam vel. Est lorem ipsum dolor sit amet.
-				Eu feugiat pretium nibh ipsum. In iaculis nunc sed augue lacus viverra. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. </p>
+				<p class="para-style"><?php echo $tracksinfo[$track]["course-para2"]; ?></p>
 
 				<h3 class="subheading"><?php echo $tracksinfo[$track]["subheading3"]; ?></h3>
-				<p class="para-style">Orci eu lobortis elementum nibh tellus molestie. Cursus in hac habitasse platea dictumst quisque sagittis purus. Tellus in metus vulputate eu. 
-				Rhoncus urna neque viverra justo nec ultrices dui sapien. Luctus accumsan tortor posuere ac ut consequat. Quam adipiscing vitae proin sagittis. 
-				Pellentesque habitant morbi tristique senectus. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Cursus metus aliquam eleifend mi in nulla. 
-				Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Et malesuada fames ac turpis egestas sed tempus urna et. </p>
+				<p class="para-style"><?php echo $tracksinfo[$track]["course-para3"]; ?></p>
 				
 				<h3 class="subheading"><?php echo $tracksinfo[$track]["subheading4"]; ?></h3>
-				<p class="para-style">Pretium viverra suspendisse potenti nullam. Id semper risus in hendrerit gravida rutrum quisque non. Accumsan lacus vel facilisis volutpat est velit. 
-				Aliquet eget sit amet tellus cras adipiscing enim. Sollicitudin aliquam ultrices sagittis orci a. Tortor dignissim convallis aenean et tortor at. 
-				Neque gravida in fermentum et sollicitudin ac orci. Etiam non quam lacus suspendisse faucibus. Egestas erat imperdiet sed euismod nisi porta lorem. </p>
+				<p class="para-style"><?php echo $tracksinfo[$track]["course-para4"]; ?></p>
 				
 				<h3 class="subheading"><?php echo $tracksinfo[$track]["subheading5"]; ?></h3>
-				<p class="para-style">Mattis molestie a iaculis at. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Est ullamcorper eget nulla facilisi etiam dignissim. 
-				Morbi tincidunt augue interdum velit euismod. Elit sed vulputate mi sit amet mauris commodo. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. 
-				Praesent tristique magna sit amet purus gravida. Quam viverra orci sagittis eu volutpat odio facilisis mauris sit. Condimentum vitae sapien pellentesque habitant morbi. </p>
+				<p class="para-style"><?php echo $tracksinfo[$track]["course-para5"]; ?></p>
 			</section>
 			<section class="track-section3" id="<?php echo $tracksinfo[$track]["background1"]; ?>">
 				<h2 class="track-heading1">Interested!! After you have signed up, come and enroll below</h2>
