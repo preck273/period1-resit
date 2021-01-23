56df743717fef3ee53ea94ab037d4ba6a555bbab
preck273 <72576947+preck273@users.noreply.github.com>
2021-01-23 09:58:16 +0100

    sql

--- includes/culinaryschool (1).sql
+++ includes/culinaryschool (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2021 at 08:32 PM
+-- Generation Time: Jan 23, 2021 at 09:53 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -32,6 +32,16 @@ CREATE TABLE `connection` (
   `student_Code` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `connection`
+--
+
+INSERT INTO `connection` (`course_code`, `student_Code`) VALUES
+(100, 1),
+(101, 1),
+(200, 2),
+(223, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +56,21 @@ CREATE TABLE `course` (
   `start_date` date DEFAULT NULL,
   `end_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_code`, `course_name`, `style`, `price`, `start_date`, `end_date`) VALUES
+(100, 'Vivamus A Tellus', 'Italian', 134, '2021-01-21', '2021-01-21'),
+(101, 'Voluptate Velit', 'Italian', 290, '2021-01-21', '2021-01-21'),
+(105, 'Ut Enim Ad Minim Veniam', 'Italian', 400, '2021-01-21', '2021-01-21'),
+(200, 'Sodales Ut Etiam', 'Asian', 134, '2021-01-23', '2021-01-23'),
+(223, 'Mattis Molestie', 'Asian', 290, '2021-01-23', '2021-01-23'),
+(225, 'Consectetur Adipiscing', 'Asian', 345, '2021-01-23', '2021-01-23'),
+(301, 'Mauris Commodo', 'Eastern European', 290, '2021-01-23', '2021-01-23'),
+(302, 'Ultrices Sagittis Orci', 'Eastern European', 134, '2021-01-23', '2021-01-23'),
+(303, 'Voluptate Velit Esse', 'Eastern European', 134, '2021-01-23', '2021-01-23');
 
 -- --------------------------------------------------------
 
@@ -67,6 +92,14 @@ CREATE TABLE `student` (
   `student_password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_Code`, `first_name`, `last_name`, `home_address`, `postal_code`, `Town`, `telephone_number`, `email_address`, `Date_of_birth`, `users_name`, `student_password`) VALUES
+(1, 'James', 'Scott', 'stationstrat 90', '7811 GS', 'Emmen', 685135674, 'jamesscott16430@gmail.com', '2021-01-21', 'james123', '$2y$10$eOw0zL.iDYStdpSZ.IMUGuMNvvqEC42z8uXFoTrga7x4GrLV.sBIS'),
+(2, 'Kelvin', 'Scott', 'stationstrat 73', '7811 GS', 'Zwolle', 685135674, 'jamesscott16430@gmail.com', '2021-01-23', 'Kelvin125', '$2y$10$ihswVJ0BJm2PngWLDI1yReX89HB38elAe./YanY.RswAuZmYse832');
+
 -- --------------------------------------------------------
 
 --
@@ -82,6 +115,13 @@ CREATE TABLE `theadmin` (
   `users_name` varchar(30) NOT NULL,
   `admin_password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `theadmin`
+--
+
+INSERT INTO `theadmin` (`admin_code`, `first_name`, `last_name`, `telephone_number`, `email_address`, `users_name`, `admin_password`) VALUES
+(1, 'James', 'Scott', 685135674, 'jamesscott16430@gmail.com', 'Admin', '$2y$10$Ds/vu8s.8Nij1.6MfGXtb.FpZ3rvDhlf7XNGTPNlsI0aFtQuyvPhq');
 
 --
 -- Indexes for dumped tables
@@ -120,19 +160,19 @@ ALTER TABLE `theadmin`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `course_code` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `course_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=304;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_Code` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `student_Code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `theadmin`
 --
 ALTER TABLE `theadmin`
-  MODIFY `admin_code` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `admin_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
